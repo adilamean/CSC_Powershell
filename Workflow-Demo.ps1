18d7b371c9fa21d09e2f8d21389599e1410c8395
Adil Rathore <adilrathore@gmail.com>
2017-01-12 02:17:41 +0530

    Added Username to Win32_ComputerSystem

--- Workflow-Demo.ps1
+++ Workflow-Demo.ps1
@@ -5,6 +5,10 @@ Demo Script for Git Workflow Demo
 #Here's the original script code
 Write-Host "This is a Git workflow demo"
 
-Get-WmiObject -Class Win32_ComputerSystem
+Get-WmiObject -Class Win32_ComputerSystem | Select Name, Manufacturer, Model, UserName
+
+
+
 
 Write-Host "Ran Command 2"
+
